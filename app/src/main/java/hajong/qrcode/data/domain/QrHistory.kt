db0cf2752dd2ff557c4407cfc3ae1b87e702400d
hajong1 <jiyong.ha@kakao.com>
2025-01-15 01:43:39 +0900

    Feat: QrHistory Entity 날짜 변환 추가

--- app/src/main/java/hajong/qrcode/data/domain/QrHistory.kt
+++ app/src/main/java/hajong/qrcode/data/domain/QrHistory.kt
@@ -3,5 +3,5 @@ package hajong.qrcode.data.domain
 data class QrHistory(
     val id: Long = 0,
     val content: String = "",
-    val timestamp: Long = System.currentTimeMillis(),
+    val timestamp: String = "",
 )
--- app/src/main/java/hajong/qrcode/data/entity/QrHistoryEntity.kt
+++ app/src/main/java/hajong/qrcode/data/entity/QrHistoryEntity.kt
@@ -2,11 +2,25 @@ package hajong.qrcode.data.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 @Entity(tableName = "qr_history")
 data class QrHistoryEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Long = 0,
     val content: String,
-    val timestamp: Long = System.currentTimeMillis()
+    val timestamp: String = toLocalDateTime()
 )
+
+fun toLocalDateTime(): String {
+    val currentMillis = LocalDateTime.now()
+        .atZone(ZoneId.systemDefault())
+        .toInstant()?.toEpochMilli() ?: 0
+
+    val currentDateTime = Instant.ofEpochMilli(currentMillis).atZone(ZoneId.systemDefault()).toLocalDateTime()
+
+    return DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분").format(currentDateTime)
+}
